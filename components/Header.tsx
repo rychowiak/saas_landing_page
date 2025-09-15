089ff9359bc94f71678ee74994419ef67b9222ae
Ricardo <ricardosepu93@gmail.com>
2025-09-14 18:39:59 -0700

    added toggle btn

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+import clsx from "clsx";
 import Image from "next/image";
 import Link from "next/link";
 import * as React from "react";
+import { useState } from "react";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="fixed top-0 left-0 z-50 w-full py-10">
       <div className="container flex h-14 items-center max-lg:px-5">
@@ -16,7 +22,12 @@ const Header = () => {
           />
         </a>
 
-        <div className="w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0">
+        <div
+          className={clsx(
+            "w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0",
+            isOpen ? "max-lg:opacity-100" : "max-lg:pointer-events-none"
+          )}
+        >
           <div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
               <ul className="flex max-lg:block max-lg:px-12">
@@ -66,6 +77,18 @@ const Header = () => {
             </nav>
           </div>
         </div>
+        <button
+          className="relative lg:hidden z-2 size-10 border-2 border-s4/25 rounded-full flex justify-center items-center"
+          onClick={() => setIsOpen((prevState) => !prevState)}
+        >
+          <Image
+            className="size-1/2 object-contain"
+            alt="toggle"
+            src={`/images/${isOpen ? "close" : "magic"}.svg`}
+            width={20}
+            height={20}
+          />
+        </button>
       </div>
     </header>
   );
