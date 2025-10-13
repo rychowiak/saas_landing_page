c31e746231863692dbcb6329c85178a684c6c160
Ricardo sepulveda <ricardosepu93@gmail.com>
2025-10-13 10:10:48 -0700

    add sliding btn

--- sections/pricing.tsx
+++ sections/pricing.tsx
@@ -2,6 +2,7 @@
 import * as React from "react";
 import { useState } from "react";
 import { clsx } from "clsx";
+import Image from "next/image";
 
 const Pricing = () => {
   const [monthly, setMonthly] = useState(false);
@@ -9,7 +10,7 @@ const Pricing = () => {
     <section>
       <div className="element">
         <div className="container">
-          <div className="max-w-960 border-2 border-amber-400">
+          <div className="max-w-960 pricing-head_before relative mx-auto border-l border-r border-s2 bg-s1/50 pb-40 pt-28 max-xl:max-w-4xl max-lg:border-none max-md:pb-32 max-md:pt-16">
             <h3 className="h3 max-lg:h4 max-md:h5 z-3 relative mx-auto mb-14 max-w-lg text-center text-p4 max-md:mb-11 mac-sm:mac-w-sm">
               Flexible pricing for teams of all sizes
             </h3>
@@ -22,10 +23,34 @@ const Pricing = () => {
               </button>
               <button
                 onClick={() => setMonthly(false)}
-                className={clsx("pricing-head_btn", !monthly && 'text-p4')}
+                className={clsx("pricing-head_btn", !monthly && "text-p4")}
               >
                 Annual
               </button>
+
+              <div
+                className={clsx(
+                  "g4 rounded-14 before:h-100 pricing-head_btn_before absolute left-2 top-2 h-[calc(100%-16px)] w-[calc(50%-8px)] overflow-hidden shadow-400 transition-transform duration-500",
+                  !monthly && "translate-x-full"
+                )}
+              />
+            </div>
+
+            <div className="pricing-bg">
+              <Image
+                src={"/images/bg-outlines.svg"}
+                alt="outline"
+                width={960}
+                height={380}
+                className="relative z-2"
+              />
+              <Image
+                src={"/images/bg-outlines-fill.png"}
+                alt="outline"
+                width={960}
+                height={380}
+                className="absolute inset-0 mix-blend-soft-light opacity-5"
+              />
             </div>
           </div>
         </div>
